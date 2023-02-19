8f50a9e794700ed855838de0e230d9a2e2b0939f
MoMoPouetDev <39879689+MoMoPouetDev@users.noreply.github.com>
2023-02-19 10:45:46 +0100

    initialization of uart folder

---
+++ source/HAL/HAL_UART/HAL_UART.c
@@ -0,0 +1,62 @@
+/*
+ * HAL_UART.c
+ *
+ *  Created on: 19 FEB 2023
+ *      Author: morgan.venandy
+ */
+
+/*--------------------------------------------------------------------------*/
+/*! ... INCLUDES ...                                                        */
+/*--------------------------------------------------------------------------*/
+#include "HAL_UART.h"
+#include "LLD_UART.h"
+
+
+/*--------------------------------------------------------------------------*/
+/* ... DATATYPES ...                                                        */
+/*--------------------------------------------------------------------------*/
+#define UART_BAUDRATE   9600U
+
+static volatile uint8_t gu8_FlagUart3;
+static volatile uint8_t gu8_FlagUart4;
+/*--------------------------------------------------------------------------*/
+/*! ... LOCAL FUNCTIONS DECLARATIONS ...                                    */
+/*--------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------*/
+/*! ... FUNCTIONS DEFINITIONS    ...                                        */
+/*--------------------------------------------------------------------------*/
+/**
+* @brief		UART callback
+* @param		e_Uart : UART number
+* @return		void
+* @details
+**/
+void HAL_UART_Callback(typ_Lld_Uart e_Uart)
+{
+	if(e_Uart == LLD_UART_UART3)
+	{
+		gu8_FlagUart3 = 1;
+	}
+	if(e_Uart == LLD_UART_UART4)
+	{
+		gu8_FlagUart4 = 1;
+	}
+
+}
+
+void HAL_UART_Init()
+{
+	gu8_FlagUart3 = 0;
+	gu8_FlagUart4 = 0;
+
+	LLD_UART_Init(LLD_UART_UART3, UART_BAUDRATE, HAL_UART_Callback);
+	LLD_UART_Init(LLD_UART_UART4, UART_BAUDRATE, HAL_UART_Callback);
+}
+
+void HAL_UART_BleInit()
+{
+	char tc_commandAT[5] = {"AT", "AT+ROLE0", "AT+UUID0xFFE0", "AT+CHAR0xFFE1", "AT+NAMEMower"};
+
+	LLD_UART_Send(LLD_UART_UART4, tc_commandAT, 5);
+}
---
+++ source/HAL/HAL_UART/HAL_UART.h
@@ -0,0 +1,15 @@
+/*
+ * HAL_I2C.h
+ *
+ *  Created on: 12 sept. 2022
+ *      Author: morgan.venandy
+ */
+
+#ifndef HAL_HAL_I2C_HAL_I2C_H_
+#define HAL_HAL_I2C_HAL_I2C_H_
+
+#include <stdint.h>
+
+void HAL_UART_Init(void);
+
+#endif /* HAL_HAL_I2C_HAL_I2C_H_ */
