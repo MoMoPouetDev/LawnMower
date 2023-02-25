41122ead223e5a2d6240bacf377870758c8ae3f2
MoMoPouetDev <39879689+MoMoPouetDev@users.noreply.github.com>
2023-02-25 19:10:20 +0100

    initialization of gps feature

--- source/HAL/HAL_UART/HAL_UART.c
+++ source/HAL/HAL_UART/HAL_UART.c
@@ -15,10 +15,8 @@
 /*--------------------------------------------------------------------------*/
 /* ... DATATYPES ...                                                        */
 /*--------------------------------------------------------------------------*/
-#define UART_BAUDRATE   9600U
-
-static volatile uint8_t gu8_FlagUart3;
-static volatile uint8_t gu8_FlagUart4;
+static volatile uint8_t gu8_FlagUartGps;
+static volatile uint8_t gu8_FlagUartBle;
 /*--------------------------------------------------------------------------*/
 /*! ... LOCAL FUNCTIONS DECLARATIONS ...                                    */
 /*--------------------------------------------------------------------------*/
@@ -34,24 +32,24 @@ static volatile uint8_t gu8_FlagUart4;
 **/
 void HAL_UART_Callback(typ_Lld_Uart e_Uart)
 {
-	if(e_Uart == LLD_UART_UART3)
+	if(e_Uart == UART_GPS)
 	{
-		gu8_FlagUart3 = 1;
+		gu8_FlagUartGps = 1;
 	}
-	if(e_Uart == LLD_UART_UART4)
+	if(e_Uart == UART_BLE)
 	{
-		gu8_FlagUart4 = 1;
+		gu8_FlagUartBle = 1;
 	}
 
 }
 
 void HAL_UART_Init()
 {
-	gu8_FlagUart3 = 0;
-	gu8_FlagUart4 = 0;
+	gu8_FlagUartGps = 0;
+	gu8_FlagUartBle = 0;
 
-	LLD_UART_Init(LLD_UART_UART3, UART_BAUDRATE, HAL_UART_Callback);
-	LLD_UART_Init(LLD_UART_UART4, UART_BAUDRATE, HAL_UART_Callback);
+	LLD_UART_Init(UART_GPS, UART_BAUDRATE, HAL_UART_Callback);
+	LLD_UART_Init(UART_BLE, UART_BAUDRATE, HAL_UART_Callback);
 }
 
 void HAL_UART_BleInit()
@@ -62,5 +60,49 @@ void HAL_UART_BleInit()
 		commandChar[] = "AT+CHAR0xFFE1",
 		commandName[] = "AT+NAMEMower";
 
-	LLD_UART_Send(LLD_UART_UART4, commandAT, 2);
+	LLD_UART_Send(UART_BLE, commandAT, 2);
+}
+
+uint8_t HAL_UART_ReceptionGPS(char* tu8_RxBuffer, uint8_t u8_size) 
+{	
+	static uint8_t u8_uartState = 0;
+	uint8_t u8_returnValue = 0;
+
+	switch (u8_uartState)
+	{
+	case 0:
+		LLD_UART_Receive(UART_GPS, tu8_RxBuffer, 1);
+		u8_uartState = 1;
+		break;
+	case 1:
+		if (gu8_FlagUartGps == 1)
+		{
+			gu8_FlagUartGps = 0;
+
+			if (tu8_RxBuffer[0] == '$')
+			{
+				LLD_UART_Receive(UART_GPS, tu8_RxBuffer, u8_size);
+				u8_uartState = 2;
+			}
+			else
+			{
+				u8_uartState = 0;
+			}
+		}
+		break;
+	case 2:
+		if (gu8_FlagUartGps == 1)
+		{
+			gu8_FlagUartGps = 0;
+			u8_uartState = 0;
+			u8_returnValue = 1;
+		}
+		break;
+	
+	default:
+		u8_uartState = 0;
+		break;
+	}
+
+	return u8_returnValue;
 }
